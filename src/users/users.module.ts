13b2184437ce784f6a3b4f62be575be614a04203
Krzysiek Wyka <krzysztof@wyka.dev>
2020-09-18 12:32:03 +0200

    Provide UserProcessor in UsersModule

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common';
+import { BullModule } from '@nestjs/bull';
 import { TypegooseModule } from 'nestjs-typegoose';
 import { UsersService } from './users.service';
 import { UserEntity } from './user.entity';
 import { UsersController } from './users.controller';
+import { UsersProcessor } from './users.processor';
 
 @Module({
-  imports: [TypegooseModule.forFeature([UserEntity])],
-  providers: [UsersService],
+  imports: [
+    TypegooseModule.forFeature([UserEntity]),
+    // TODO: Read config values!!
+    BullModule.registerQueue({
+      name: 'users',
+      redis: {
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
+  ],
+  providers: [UsersService, UsersProcessor],
   controllers: [UsersController],
 })
 export class UsersModule {}
