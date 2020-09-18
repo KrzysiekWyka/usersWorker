2b2aff439b1f17c97d1695431737aaa57422b9eb
Krzysiek Wyka <krzysztof@wyka.dev>
2020-09-18 13:08:34 +0200

    Import TypeOrmModule module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,31 @@
 import { Module } from '@nestjs/common';
 import * as Joi from '@hapi/joi';
-import { TypegooseModule } from 'nestjs-typegoose';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    // TODO: Read connection string from config file
-    TypegooseModule.forRootAsync({
+    TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({
-        uri: config.get<string>('MONGO_URI'),
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
+        type: 'mysql',
+        host: config.get<string>('DATABASE_HOST'),
+        port: 3306,
+        username: config.get<string>('DATABASE_USERNAME'),
+        password: config.get<string>('DATABASE_PASSWORD'),
+        database: config.get<string>('DATABASE_NAME'),
+        entities: [],
+        synchronize: true,
       }),
     }),
     ConfigModule.forRoot({
       validationSchema: Joi.object({
-        MONGO_URI: Joi.string().default(
-          'mongodb://localhost:27017/users-worker',
-        ),
+        DATABASE_HOST: Joi.string().default('localhost'),
+        DATABASE_USERNAME: Joi.string().default('root'),
+        DATABASE_PASSWORD: Joi.string().default('users-worker'),
+        DATABASE_NAME: Joi.string().default('users-worker'),
         REDIS_HOST: Joi.string().default('localhost'),
         REDIS_PORT: Joi.number().default(6379),
       }),
