f7529cc1ae6cfe4f7accfad05d5fd63219e763b2
Krzysiek Wyka <krzysztof@wyka.dev>
2020-09-18 11:51:30 +0200

    Combine all users components in UsersModule

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { UsersService } from './users.service';
+import { UserEntity } from './user.entity';
+import { UsersController } from './users.controller';
 
-@Module({})
+@Module({
+  imports: [TypegooseModule.forFeature([UserEntity])],
+  providers: [UsersService],
+  controllers: [UsersController],
+})
 export class UsersModule {}
