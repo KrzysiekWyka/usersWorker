b537a7573ca17d091b2d30f62d81be3e8de22bc7
Krzysiek Wyka <krzysztof@wyka.dev>
2020-09-18 12:32:18 +0200

    Create UsersProcessor

---
+++ src/users/users.processor.ts
@@ -0,0 +1,22 @@
+import { Process, Processor } from '@nestjs/bull';
+import { UsersService } from './users.service';
+import { Job } from 'bull';
+import { UserEntity } from './user.entity';
+import { Logger } from '@nestjs/common';
+
+@Processor('users')
+export class UsersProcessor {
+  // TODO: In the future winston or pino should be consider
+  private readonly logger = new Logger(UsersProcessor.name);
+
+  constructor(private readonly usersService: UsersService) {}
+
+  @Process('transcode')
+  async transcode(job: Job<UserEntity>) {
+    this.logger.setContext(this.transcode.name);
+
+    this.logger.debug(`Job: ${job.id} Payload: ${JSON.stringify(job.data)}`);
+
+    return this.usersService.consumeUser(job.data);
+  }
+}
