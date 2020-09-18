5eb19be77c5d278a04ddc4ecc10eb99bef2f9c3f
Krzysiek Wyka <krzysztof@wyka.dev>
2020-09-18 12:59:07 +0200

    Load Typegoose connection string from config & validate config values

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,31 @@
 import { Module } from '@nestjs/common';
+import * as Joi from '@hapi/joi';
 import { TypegooseModule } from 'nestjs-typegoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     // TODO: Read connection string from config file
-    TypegooseModule.forRoot('mongodb://localhost:27017/users-worker', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+    TypegooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        uri: config.get<string>('MONGO_URI'),
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }),
+    }),
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        MONGO_URI: Joi.string().default(
+          'mongodb://localhost:27017/users-worker',
+        ),
+        REDIS_HOST: Joi.string().default('localhost'),
+        REDIS_PORT: Joi.number().default(6379),
+      }),
     }),
     UsersModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
